d59a85ddd05c64189ae50dd53080b369e550b9e6
kcl886 <652370637@qq.com>
2020-08-22 13:56:26 +0800

    first commit

---
+++ 第6章 高级IO函数/cmake-build-debug/CMakeFiles/6_IO.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "6_IO"
+  "6_IO.pdb"
+  "CMakeFiles/6_IO.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/6_IO.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
